cf1c324107e16101945572eb5bc229f1cc2338d0
Shannon Woods <45649247+n0nnahs@users.noreply.github.com>
2021-05-08 21:26:44 -0400

    added photos

---
+++ src/app/shared/models/polish.ts
@@ -0,0 +1,10 @@
+export interface polish {
+  id: string;
+  brand: string;
+  collection: string;
+  type: string[];
+  color: string[];
+  purchase_date: string;
+  description: string;
+  swatched: string;
+}
